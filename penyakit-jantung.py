1b085e21e9d30543e42ba39f89d8743b1282530f
adeandreass <121536792+adeandreass@users.noreply.github.com>
2023-11-25 09:08:24 +0700

    Add files via upload

--- penyakit-jantung.py
+++ penyakit-jantung.py
@@ -16,11 +16,11 @@ with col1:
     age = st.text_input('Umur')
     impulse = st.text_input('Detak Jantung(bpm)')
     pressurelow = st.text_input('Tekanan Darah Diastolik(mmHG)')
-    glucose = st.text_input('Gula Darah(mg/dL)')
+    kcm = st.text_input('Kadar CK-MB(unit/liter)')
 with col2:
     gender = st.text_input('Jenis Kelamin (Masukkan 1 jika laki-laki, 0 jika perempuan)')
     pressurehight = st.text_input('Tekanan Darah Sistolik(mmHG)')
-    kcm = st.text_input('Kadar CK-MB(unit/liter)')
+    glucose = st.text_input('Gula Darah(mg/dL)')
     troponin = st.text_input('Kadar Troponin(ng/mL)')
 
 # Code for prediction
@@ -38,9 +38,9 @@ if st.button('Prediksi Penyakit Jantung'):
 
     # Tampilkan hasil prediksi dengan probabilitas
     if heart_prediction[0] == 1:
-        heart_diagnosis = 'Pasien Terindikasi Penyakit Jantung'
+        heart_diagnosis = st.warning('Pasien Terindikasi Penyakit Jantung')
     else:
-        heart_diagnosis = 'Pasien Tidak Terindikasi Penyakit Jantung'
+        heart_diagnosis = st.success('Pasien Tidak Terindikasi Penyakit Jantung')
 
 # Tampilkan ringkasan input yang dimasukkan oleh pengguna
 st.subheader('Ringkasan Input:')
@@ -50,24 +50,6 @@ st.write('Jenis Kelamin:', gender_text)
 st.write('Detak Jantung:', impulse, 'bpm')
 st.write('Tekanan Darah Sistolik:', pressurehight, 'mmHG')
 st.write('Tekanan Darah Diastolik:', pressurelow, 'mmHG')
-st.write('Kadar CK-MB:', kcm, 'unit/Liter darah')
 st.write('Gula Darah:', glucose, 'mg/dL')
+st.write('Kadar CK-MB:', kcm, 'unit/Liter darah')
 st.write('Troponin:', troponin, 'ng/mL')
-
-# Tampilkan hasil diagnosis
-if heart_diagnosis:
-    st.success(heart_diagnosis)
-
-
-st.subheader('Tentang Model:')
-st.write('Model ini dibuat dengan algoritma C4.5.')
-
-#Classification Report
-st.write('Model ini memiliki tingkat akurasi 98%. Seperti yang bisa dilihat dalam classification report:')
-image_path = 'class.png'
-st.image(image_path, caption='Classification Report', use_column_width=True)
-
-#Pohon Keputusan
-st.write('Berikut adalah pohon keputusan dari model ini:')
-image_path = 'pohon_keputusan.png'
-st.image(image_path, caption='Pohon Keputusan', use_column_width=True)
